5317c9e10caa801aee8e20a0bd229bb9ec61bef1
Alban Pipon <alban.pipon@trivadis.com>
2017-03-10 09:14:59 +0100

    users-list interaction

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'users-list',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListComponent implements OnInit {
 
-  private users: Array<string> = ["Alban"]
+  @Input()
+  private users: Array<string>
 
   constructor() { }
 
