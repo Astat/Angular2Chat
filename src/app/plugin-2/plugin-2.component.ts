f1c9f53ba01c73f295ac540ef2d68766919fa4ef
Pascal Lovy <pascal.lovy@trivadis.com>
2017-04-06 17:25:59 +0200

    Plugin meteo avec "/meteo geneva"

--- src/app/plugin-2/plugin-2.component.ts
+++ src/app/plugin-2/plugin-2.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { PluginTemplateComponent } from '../plugin-template/plugin-template.component'
+import {Component} from "@angular/core";
+import {Http} from "@angular/http";
+import {PluginTemplateComponent} from "../plugin-template/plugin-template.component";
 
 @Component({
   selector: 'plugin-2',
@@ -8,14 +9,37 @@ import { PluginTemplateComponent } from '../plugin-template/plugin-template.comp
 })
 export class Plugin2Component extends PluginTemplateComponent {
 
-  constructor() {
-    super()
+  constructor(private http: Http) {
+    super();
   }
 
+  private woeid: number;
+  private title: string;
+  private weatherStateAbbr: string;
+  private weatherStateName: string;
+
   process(command: string, value: string, author: string) {
-    if (command != "plug2") {
-      return
+    if (command != "meteo") {
+      return;
     }
-    this.intercept()
+
+    this.http
+      .get("https://www.metaweather.com/api/location/search/?query=" + value)
+      .subscribe(response => {
+        let json = response.json();
+        this.woeid = json[0].woeid;
+        this.title = json[0].title;
+
+        this.http
+          .get("https://www.metaweather.com/api/location/" + this.woeid)
+          .subscribe(response => {
+            let json = response.json();
+            this.weatherStateAbbr = json.consolidated_weather[0].weather_state_abbr;
+            this.weatherStateName = json.consolidated_weather[0].weather_state_name;
+          });
+
+      });
+
+    this.intercept();
   }
 }
